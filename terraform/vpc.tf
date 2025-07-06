d461f52d4ae785c2a44c6304d03392a9ac5de650
Rayan <rayan@example.com>
2025-07-06 15:38:05 +0100

    aaa

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,10 +2,13 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 data "aws_subnet" "default" {
-  id = data.aws_subnet_ids.default.ids[0]
+  id = data.aws_subnets.default.ids[0]
 }
