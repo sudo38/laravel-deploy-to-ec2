d28b4e7024a35a2af95de7d3d6dc94cc4bd39e02
Rayan <rayan@example.com>
2025-07-06 21:02:52 +0100

    aaa

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,22 @@ resource "aws_instance" "laravel_server" {
 
   subnet_id                   = data.aws_subnet.default.id
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
+  vpc_security_group_ids = [data.aws_security_group.allow_http_ssh.id]
+
 
   tags = {
     Name = "laravel-deploy-to-ec2"
+  }
+}
+
+data "aws_security_group" "allow_http_ssh" {
+  filter {
+    name   = "group-name"
+    values = ["allow_http_ssh"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
