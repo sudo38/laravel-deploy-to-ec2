8b3231400502d6d489f9a629c6417ad81fb091d8
Rayan <rayan@example.com>
2025-07-10 20:56:20 +0100

    aaa

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "rayan-terraform-state-bucket"
-    key            = "dev/terraform.tfstate"
+    key            = "terraform/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
