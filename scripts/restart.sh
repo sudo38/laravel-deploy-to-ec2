0b2be808b3ee9e72b58d0309fc3f6fe61d4d330f
sudo38 <rayan@example.com>
2025-07-11 22:22:47 +0100

    aaa

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-cd /home/ec2-user/deploy-temp
-
-# تحديث composer
-composer install --no-dev --optimize-autoloader
-
-# إعداد الصلاحيات
-chmod -R 775 storage
-chmod -R 775 bootstrap/cache
-
-# إعداد .env و Key
-cp .env.example .env
-php artisan key:generate
-
-# تأكد أن السيرفر يعمل مثلاً:
-sudo systemctl restart apache2
